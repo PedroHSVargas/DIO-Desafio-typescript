77d95c8216d047494b00a9b17f892c56bd55519b
Pedro Vargas <pedroh_vargas@hotmail.com>
2022-08-07 04:47:01 -0300

    start project

---
+++ exemplos/objetos-listas.ts
@@ -0,0 +1,67 @@
+const pessoa = {
+    nome: "Mariana",
+    idade: 28,
+    profissao: "desenvolvedora"
+}
+
+const andre: { nome: string, idade: number, profissao: string} = {
+    nome: 'Andre',
+    idade: 25,
+    profissao: 'pintor' 
+}
+
+const paula: { nome: string, idade: number, profissao: string} = {
+    nome: 'Paula',
+    idade: 25,
+    profissao: 'Desenvolvedora' 
+}
+
+enum Profissao {
+    Professora,
+    Atriz,
+    Desenvolvedora,
+    Atleta
+}
+
+interface Pessoa {
+    nome: string,
+    idade: number,
+    profissao?: Profissao,
+}
+
+interface Estudante extends Pessoa {
+    materias: string[]
+}
+
+const vanessa: Pessoa = {
+    nome: 'Vanessa',
+    idade: 23,
+    profissao: Profissao.Desenvolvedora 
+}
+
+const maria: Pessoa = {
+    nome: 'Maria',
+    idade: 23,
+    profissao: Profissao.Desenvolvedora 
+}
+
+const jessica: Estudante = {
+    nome: 'Jessica',
+    idade: 28,
+    profissao: Profissao.Desenvolvedora,
+    materias: ['Matematica discreta', 'programção']
+}
+
+const monica: Estudante = {
+    nome: 'Monica',
+    idade: 28,
+    materias: ['Matematica discreta', 'programção']
+}
+
+function lista(lista: string[]) {
+    for (const item of lista) {
+        console.log('- ', item);
+    }
+}
+
+lista(monica.materias);
